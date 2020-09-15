56f1ebcdbb8a9012279fb32e577fd07337c24a27
James Robert <me@jiaaro.com>
2020-09-15 08:47:33 -0500

    try cargo culting the -G flag from windows

--- build_lovejs.sh
+++ build_lovejs.sh
@@ -8,7 +8,7 @@ source ~/git/emsdk/emsdk_env.sh
 
 (
   cd build/release
-  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
+  emcmake cmake /Users/dkhachat/git/megasource -G "Unix Makefiles" -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release
   emmake make -j 6
   cp love/love.js* ../../src/release
   cp love/love.wasm ../../src/release
@@ -17,7 +17,7 @@ source ~/git/emsdk/emsdk_env.sh
 
 (
   cd build/compat
-  emcmake cmake /Users/dkhachat/git/megasource -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1
+  emcmake cmake /Users/dkhachat/git/megasource -G "Unix Makefiles" -DLOVE_JIT=0 -DCMAKE_BUILD_TYPE=Release -DLOVEJS_COMPAT=1
   emmake make -j 6
   cp love/love.js* ../../src/compat
   cp love/love.wasm ../../src/compat
